b1ea428c5d2690a3769a867e34f0c55692c4a721
HariKiran <harikiran.kante@gmail.com>
2022-01-26 17:20:58 +0530

    skills section completed

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -1,5 +1,25 @@
 var body = document.getElementsByTagName("body")[0];
-// body.style.display = "none";
+body.style.display = "none";
 window.onload = function () {
   body.style.display = "block";
 };
+
+let observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      var circles = document.getElementsByClassName("circle");
+      // console.log(entry);
+      for (let circle of circles) {
+        if (entry.isIntersecting) {
+          circle.style.animation = "fill 2s linear forwards";
+        } else {
+          circle.style.animation = "none";
+        }
+      }
+    });
+  },
+  { threshold: 0.25 }
+);
+
+let skills = document.querySelector(".skills");
+observer.observe(skills);
